12857026f1b513b26565da34f55b59c84f02f397
Deepjyoti Baishya <deepjyoti120281@gmail.com>
2022-05-13 23:43:46 +0530

    new again start

--- src/main.js
+++ src/main.js
@@ -6,6 +6,4 @@ import './index.css'
 
 const app = createApp(App)
 app.use(router)
-app.mount('#app')
-import VueCompositionApi from '@vue/composition-api'
-Vue.use(VueCompositionApi)
+app.mount('#app') 
--- src/partials/dashboard/User.vue
+++ src/partials/dashboard/User.vue
@@ -5,23 +5,25 @@
 </template>
 
 <script>
-import { auth } from "../../firebase";
-import { ref } from "@vue/composition-api";
+import { auth } from '../../firebase';
+import { ref } from '@vue/composition-api';
 
 export default {
   setup() {
-    const user = ref(null); 
+
+    const user = ref(null);
     const unsubscribe = auth.onAuthStateChanged(
-      (firebaseUser) => (user.value = firebaseUser)
-    );
+        
+                            firebaseUser =>  user.value = firebaseUser
+                        );
     return {
       user,
       unsubscribe,
-    };
+    }
   },
 
-  destroyed() {
-    this.unsubscribe();
-  },
-};
+  unmounted() {
+    this.unsubscribe()
+  }
+}
 </script>
